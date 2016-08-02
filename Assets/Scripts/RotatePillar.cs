a9b8b8de1be51e3d33a2879f9fb532c251a9b990
Deepak Ravindran <dpkravi@gmail.com>
2016-08-02 13:21:45 -0400

    Added a few prefabs
    3 different types of trap prefabs added

---
+++ Assets/Scripts/RotatePillar.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePillar : MonoBehaviour {
+
+    public GameObject rotatePoint;
+    public float speed = 10f;
+    void Update()
+    {
+        transform.RotateAround(rotatePoint.transform.position ,Vector3.up, speed * Time.deltaTime);
+    }
+}
