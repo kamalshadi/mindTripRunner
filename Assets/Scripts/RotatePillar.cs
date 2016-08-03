696a02568509dec35c07bae331587f8c90ae3df9
Deepak Ravindran <dpkravi@gmail.com>
2016-08-03 01:24:34 -0400

    20 percent done
    First part of the level is done

--- Assets/Scripts/RotatePillar.cs
+++ Assets/Scripts/RotatePillar.cs
@@ -5,8 +5,19 @@ public class RotatePillar : MonoBehaviour {
 
     public GameObject rotatePoint;
     public float speed = 10f;
+
+    // 0 is for Up rotation, 1 is for Right Rotation.
+    public float axis = 0;
     void Update()
     {
-        transform.RotateAround(rotatePoint.transform.position ,Vector3.up, speed * Time.deltaTime);
+        if (axis == 0)
+        {
+            transform.RotateAround(rotatePoint.transform.position, Vector3.up, speed * Time.deltaTime);
+        }
+        if (axis == 1)
+        {
+            transform.RotateAround(rotatePoint.transform.position, Vector3.right, speed * Time.deltaTime);
+        }
+
     }
 }
